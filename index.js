8bc0aa2f7e97bafa8edc3c31b32cf2d17b29936e
shamehaera9 <shamehaera9@gmail.com>
2024-04-28 09:27:01 +0600

    added a get for fatch data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,14 @@ app.get('/tour',async(req,res)=>{
   const cursor = tourCollection.find();
   const result = await cursor.toArray();
   res.send(result);
+
+})
+
+app.get('/tour/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:new ObjectId(id)}
+  const result = await tourCollection.findOne(query)
+res.send(result)
 })
 
 
